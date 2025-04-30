fde9926a9fecb3baf3e2fa7e4effd44afebef7ca
unreal3579 <unreal3579@bk.ru>
2025-04-30 08:40:30 +0300

    Update pb_ziyi.mk

--- pb_ziyi.mk
+++ pb_ziyi.mk
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/pb/config/common.mk)
 PRODUCT_RELEASE_NAME := ziyi
 
 PRODUCT_DEVICE := ziyi
-PRODUCT_NAME := twrp_ziyi
+PRODUCT_NAME := pb_ziyi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi 13 Lite
 PRODUCT_MANUFACTURER := Xiaomi
