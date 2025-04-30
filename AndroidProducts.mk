86f3e2915cbce3a77c39ba86d8a69a8a8b83de8c
unreal3579 <unreal3579@bk.ru>
2025-04-30 08:25:13 +0300

    test

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_ziyi.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/pb_ziyi.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_ziyi-user \
-    twrp_ziyi-userdebug \
-    twrp_ziyi-eng
+    pb_ziyi-user \
+    pb_ziyi-userdebug \
+    pb_ziyi-eng
